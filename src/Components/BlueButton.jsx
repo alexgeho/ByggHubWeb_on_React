361a8342ec2af158bfdfbb15d9e214fcf925bd19
Alexander Gerhard <870717ag@gmail.com>
2025-10-07 11:33:52 +0200

    feat: add BlueButton and TitleAndSubtitle components with styles

---
+++ src/Components/BlueButton.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Button} from "react-bootstrap";
+
+export default class BlueButton extends Component {
+    render() {
+        return (
+         <Button className="BlueButton">
+             Get Started For Free
+         </Button>
+        );
+    }
+}
+
